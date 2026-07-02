ab3fee7c59f6af0a06088dfa16bbd9f7e439ee90
4lex8lue <ab.80x08@gmail.com>
2026-07-02 04:10:40 +0000

    yyh

--- hello.c
+++ hello.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 int main() {
-    printf("Wassup my nigga...\n");
+    printf("wa ssupp my niggaa...\n");
     printf("Hows it goin nigga?\n");
-
+    /* These are multiline comments.
+    i can type whatever. no need for extra things. like extra slashes. like the one at the bottom. */
     //These are just texts...
         return 0;
         }
-        
